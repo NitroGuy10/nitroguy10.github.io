e141c1a7e99f27ef7799256c8050d7271017ce11
NitroGuy10 <naviyer10@gmail.com>
2023-01-08 22:29:34 -0600

    More eslint configuration

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,17 @@
 </script>
 
 <template>
-  <Body class="bg-zinc-800 text-zinc-300"></Body>
   <div id="root">
-    <div id="page">  
+  <Body class="bg-zinc-800 text-zinc-300" />
+    <div id="page">
       <div id="content" class="mx-auto max-w-4xl">
         <h1 class="text-9xl font-bold text-center">NitroGuy</h1>
         <p>text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and text and </p>
-        <Cool num="1"/>
-        <Cool num="2"/>
-        <Cool num="3"/>
-        <Cool num="4"/>
-        <Cool num="5" punct="!!!"/>
+        <Cool num="1" />
+        <Cool num="2" />
+        <Cool num="3" />
+        <Cool num="4" />
+        <Cool num="5" punct="!!!" />
 
         <ul>
           <li v-for="song of SONGS"><NuxtLink :to="'./songs/' + song" class="text-blue-400 underline">{{ song }}</NuxtLink></li>
