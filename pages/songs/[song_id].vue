d66970f19128be60f2a9c39e10a7b0756187e24b
NitroGuy10 <naviyer10@gmail.com>
2023-03-12 20:43:25 -0500

    Started song pages

--- pages/songs/[song_id].vue
+++ pages/songs/[song_id].vue
@@ -1,11 +1,80 @@
 <script lang="ts" setup>
-    const route = useRoute();
+  import discography from "@/data/song_data.json";
 
-    const songId = route.params.song_id;
+  const route = useRoute();
+
+  const songId = route.params.song_id;
+  const songData = discography["songs"][songId];
+
+  definePageMeta({ layout: "music" });
 </script>
 
 <template>
     <div>
-      <h1>{{ songId }} is such a cool song!!!</h1>
+      <div class="grid grid-cols-2">
+        <div>
+          <h1 class="text-5xl font-bold">{{ songData.name }}</h1>
+          <p>{{ songData.description }}</p>
+          <p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p>
+        </div>
+        <div>
+          <img :src="'/images/music/' + songData.coverLink">
+        </div>
+      </div>
+      <p>{{ songData }}</p>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
     </div>
 </template>
