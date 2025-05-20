419795ad81fd63bb64e4aad63688b9e444405b63
NitroGuy10 <naviyer10@gmail.com>
2025-05-19 20:03:47 -0500

    Try again

--- error.vue
+++ error.vue
@@ -11,17 +11,17 @@
 
 <template>
   <div>
-    <Body class="bg-zinc-900 select-none  doesnt-load-for-some-reason" />
+    <Body class="bg-zinc-900 select-none" />
 
-    <div v-for="num in fourtyTimes" class="absolute top-0 left-0 bottom-0 right-0 spin-cw" :style="'animation-delay: -' + (num / 4) + 's;'">
+    <div v-for="num in fourtyTimes" class="absolute top-0 left-0 bottom-0 right-0 spin-cw text-zinc-300" :style="'animation-delay: -' + (num / 4) + 's;'">
       <p class="text-[3vh] hidden md:inline absolute top-[10%] left-1/2 -translate-x-1/2">{{ props.error.statusCode }}</p>
     </div>
-    <div v-for="num in fourtyTimes" class="absolute top-0 left-0 bottom-0 right-0 spin-ccw" :style="'animation-delay: -' + (num / 4) + 's;'">
+    <div v-for="num in fourtyTimes" class="absolute top-0 left-0 bottom-0 right-0 spin-ccw text-zinc-300" :style="'animation-delay: -' + (num / 4) + 's;'">
       <p class="text-[3vh] hidden md:inline absolute top-[5%] left-1/2 -translate-x-1/2">{{ props.error.statusCode }}</p>
     </div>
 
     <div class="flex justify-center items-center h-screen">
-      <div class="text-center">
+      <div class="text-center text-zinc-300">
         <h1 class="text-9xl mb-8">{{ props.error.statusCode === 404 ? "404" : "ERROR" }}</h1>
         <h1 class="text-5xl sm:text-7xl">LOLOLOLOL</h1>
       </div>
@@ -46,9 +46,7 @@
   .spin-ccw {
     animation: spin-ccw 10s linear infinite;
   }
-
-  .doesnt-load-for-some-reason {
-    color: #d4d4d8;
+  body {
     overflow: hidden;
   }
 </style>
